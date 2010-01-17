ad4e83b484f059978669ad8355ab0975417613f8
Alex Vollmer <alex.vollmer@gmail.com>
2010-01-17 15:02:00 -0800

    Add Pygments support for syntax highlighting.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,1 +1,2 @@
 include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Tagging
---
+++ lib/highlighter.rb
@@ -0,0 +1,19 @@
+module Nanoc3::Helpers::Filtering
+
+  def highlight(syntax, &block)
+    # Seamlessly ripped off from the filter method...
+    # Capture block
+    data = capture(&block)
+    # Reconvert
+    data.gsub! /<%/, ''
+    # Filter captured data
+    filtered_data = "\n<notextile>"+Albino.colorize(data, syntax)+"</notextile>\n" rescue data
+    # Append filtered data to buffer
+    buffer = eval('_erbout', block.binding)
+    buffer << filtered_data
+  end
+
+end
+
+include Nanoc3::Helpers::Filtering
+
