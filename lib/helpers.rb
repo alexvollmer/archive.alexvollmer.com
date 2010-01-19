d53d56349d97e56e07edf549bd11ef49c4c17226
Alex Vollmer <alex.vollmer@gmail.com>
2010-01-19 09:26:36 -0800

    Re-implemented Atom as a page to be compiled.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,5 @@
+require "nokogiri"
+
 module Nanoc3::Helpers::Blogging
   # this is an awful, awful hack because the blogging module assumes
   # that the :created_at attribute is a string and might not already
@@ -6,4 +8,32 @@ module Nanoc3::Helpers::Blogging
   def convert_items(items)
     items.each { |i| i[:created_at] = i[:created_at].to_s }
   end
+end
+
+def format_rfc822_date(date)
+  date.strftime("%Y-%m-%dT%H:%M:%S%Z")
+end
+
+def get_post_body(post)
+  doc = Nokogiri(open("output/posts/#{post.full_path}/index.html").read)
+  (doc/"body")
+end
+
+def find_text_node(nodes)
+  if nodes.empty?
+    nil
+  elsif text = nodes.find{ |x| x.name == "text" && x.text !~ /\n+/ }
+    return text
+  else
+    find_text_node(nodes.children)
+  end
+end
+
+def include_post_summary(post)
+  children = get_post_body(post).children
+  find_text_node(children)
+end
+
+def include_post_body(post)
+  get_post_body(post).children.map{ |x| x.to_s }.join(" ")
 end
