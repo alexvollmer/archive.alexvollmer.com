869012ba1b50857b26a17efe369701ed230d6232
Alex Vollmer <alex.vollmer@gmail.com>
2010-01-29 22:30:38 -0800

    Tags and by-date pages now with home layout.

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,4 +1,16 @@
+require "erb"
+require "pathname"
 require "nanoc3/cli"
+
+layouts = Pathname("layouts")
+home_erb = ERB.new(open(layouts + "home.html.erb").readlines[2..-1].join,
+                   nil, nil, "@home")
+
+eval <<-EOF
+def render_home_template
+  #{home_erb.src}
+end
+EOF
 
 module SiteUtils
 
@@ -28,12 +40,16 @@ module SiteUtils
     tag_content = ["<h1>All Tags</h1>"]
     tag_content << "<ul>"
     tags_start = Time.now
+
     by_tag.keys.sort_by { |t| t.downcase }.each do |tag|
       tag_start = Time.now
-      posts = by_tag[tag]
+      @posts = by_tag[tag]
+      plural = posts.size == 1 ? "post" : "posts"
+      @title = "#{@posts.size} #{plural} tagged with  <em>#{tag}</em>"
+      @parent_link = %Q{<a href="/tags/">all tags</a>}
+
       tag_content << list_item("/tags/#{tag}/", tag, "<em>(#{posts.size})</em>")
 
-      plural = posts.size == 1 ? "post" : "posts"
       tag_page = ["#{posts.size} #{plural} tagged with <em>#{tag}</em> | <a href='/tags/'>all tags</a>"]
       tag_page << "<ul>"
       posts.each do |post|
@@ -41,11 +57,16 @@ module SiteUtils
                               post.title,
                               " (#{post.display_date})")
       end
+
       tag_page << "</ul>"
-      write_output(tag_output + "#{tag}/index.html", tag_page.join("\n"), tag_start)
+      write_output(tag_output + "#{tag}/index.html",
+                   render_home_template { tag_page.join("\n") },
+                   tag_start)
     end
     tag_content << "</ul>"
-    write_output(tag_output + "index.html", tag_content.join("\n"), tags_start)
+    write_output(tag_output + "index.html",
+                 render_home_template { tag_content.join("\n") },
+                 tags_start)
   end
 
   def write_dates_pages(by_date)
@@ -71,10 +92,14 @@ module SiteUtils
         end
         content << "</ul>"
       end
-      write_output(posts_output + "#{year}/index.html", content.join("\n"), year_start)
+      write_output(posts_output + "#{year}/index.html",
+                   render_home_template { content.join("\n") },
+                   year_start)
     end
     year_index << "</ul>"
-    write_output(posts_output + "index.html", year_index.join("\n"), years_start)
+    write_output(posts_output + "index.html",
+                 render_home_template { year_index.join("\n") },
+                 years_start)
   end
 
   def write_output(path, contents, start_time)
@@ -85,4 +110,19 @@ module SiteUtils
     Nanoc3::CLI::Logger.instance.file(:high, :create, path, Time.now - start_time)
   end
 
+  def write(out_path, template)
+    out_path = Pathname("output") + out_path unless Pathname === out_path
+    start_time = Time.now
+    out_path.parent.mkpath
+
+    type = out_path.file? ? :update : :create
+    out_path.open("w") do |f|
+      f << render_home_template do
+        erb = ERB.new(open("templates/" + template).read, nil, nil, "@final_out")
+        erb.result(binding)
+      end
+    end
+
+    Nanoc3::CLI::Logger.instance.file(:high, type, out_path, Time.now - start_time)
+  end
 end
--- tasks/site.rake
+++ tasks/site.rake
@@ -1,4 +1,5 @@
 require "erb"
+require "ostruct"
 require "pathname"
 require "pp"
 require "lib/utils"
@@ -10,14 +11,55 @@ namespace :site do
   task :load_site do
     @site = Nanoc3::Site.new('.')
     @site.load_data
+    @indexed_posts = collect_posts_by_tags_and_year(@site)
   end
 
-  desc "Generate index pages by tag and date"
-  task :tags_and_dates => :load_site do
+  desc "Generate index pages by tag"
+  task :tags => :load_site do
     @site.load_data
-    posts = collect_posts_by_tags_and_year(@site)
-    write_tags_pages(posts[:tags])
-    write_dates_pages(posts[:dates])
+
+    @items = @indexed_posts[:tags].keys.sort_by { |x| x.downcase }.map do |tag|
+      OpenStruct.new(:full_path => "/tags/#{tag}/",
+                     :title => tag,
+                     :size => @indexed_posts[:tags][tag].size)
+    end
+    @title = "All tags"
+    write("tags/index.html", "items.html.erb")
+
+    @indexed_posts[:tags].each do |@tag, @posts|
+      plural = @posts.size == 1 ? "post" : "posts"
+      @title = "#{@posts.size} #{plural} tagged with  <em>#{@tag}</em>"
+      @parent_link = %Q{<a href="/tags/">all tags</a>}
+      write("tags/#{@tag}/index.html", "posts.html.erb")
+    end
+  end
+
+  desc "Generate index pages by year and year/month"
+  task :dates => :load_site do
+    @items = @indexed_posts[:dates].keys.sort_by { |x| x.to_i }.map do |year|
+      OpenStruct.new(:full_path => "/posts/#{year}/",
+                     :title => year,
+                     :size => @indexed_posts[:dates][year].values.inject(0) { |m,o| m += o.size })
+    end
+    @title = "All Years"
+    @parent_link = %Q[<a href="/posts/">all years</a>]
+    write("posts/index.html", "items.html.erb")
+
+    @indexed_posts[:dates].each do |year, by_month|
+      @items = by_month.keys.sort_by { |x| x.to_i }.map do |month|
+        @title = "Posts for #{Date::MONTHNAMES[month]}, #{year}"
+        @parent_link = %Q[<a href="/posts/#{year}/">all posts for #{year}</a>]
+        @posts = by_month[month]
+        write(sprintf("posts/%04d/%02d/index.html", year, month), "posts.html.erb")
+
+        OpenStruct.new(:full_path => sprintf("/posts/%04d/%02d/", year, month),
+                       :title => Date::MONTHNAMES[month],
+                       :size => by_month[month].size)
+      end
+      @title = "Posts for #{year}"
+      @parent_link = %Q[<a href="/posts/">posts for all years</a>]
+      write("posts/#{year}/index.html", "items.html.erb")
+    end
   end
 
   desc "Generate Atom and RSS feeds"
@@ -47,6 +89,6 @@ namespace :site do
   end
 
   desc "Build the whole damn site"
-  task :build => [ :compile, :copy_assets, :tags_and_dates ]
+  task :build => [ :compile, :copy_assets, :tags, :dates ]
 end
 
