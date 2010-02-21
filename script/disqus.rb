88d3dde28fb2759fa2b9a783e5429fc069e360da
Alex Vollmer <alex.vollmer@gmail.com>
2010-02-21 12:17:47 -0800

    Yet more Disqus work. Not quite there yet.

--- script/disqus.rb
+++ script/disqus.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
-require "rubygems"
-require "httparty"
+require 'ostruct'
+require 'rubygems'
+require 'httparty'
 
 class Disqus
   include HTTParty
@@ -11,81 +12,80 @@ class Disqus
 
   attr_accessor :api_key, :forum_id
 
-  def initialize(api_key)
+  def initialize(api_key, forum_id)
     @api_key = api_key
+    @forum_id = forum_id
   end
 
   def forum_list
-    @forum_list ||= self.class.get("/get_forum_list", :query => {
-                                     :user_api_key => @api_key
-                                   })["message"]
+    structify(self.class.get("/get_forum_list", :query => {
+                               :user_api_key => @api_key
+                             })["message"])
   end
 
-  def forum_api_key(forum_id)
-    if forum_id != self.forum_id || !@forum_api_key
-      @forum_api_key = self.class.get("/get_forum_api_key", :query => {
-                                          :user_api_key => @api_key,
-                                          :forum_id => forum_id
-                                        })["message"]
-      self.forum_id = forum_id
-    end
-    @forum_api_key
+  def forum_api_key
+    @forum_api_key ||= self.class.get("/get_forum_api_key", :query => {
+                                        :user_api_key => @api_key,
+                                        :forum_id => self.forum_id
+                                      })["message"]
   end
 
-  def forum_posts(forum_id=self.forum_id)
-    self.class.get("/get_forum_posts", :query => {
-                     :forum_id => forum_id,
-                     :forum_api_key => self.forum_api_key(forum_id)
-                   })["message"]
+  def forum_posts
+    structify(self.class.get("/get_forum_posts", :query => {
+                               :forum_id => self.forum_id,
+                               :forum_api_key => self.forum_api_key
+                             })["message"])
   end
 
-  def thread_list(forum_id=self.forum_id, limit=25)
-    self.class.get("/get_thread_list", :query => {
-                     :forum_id => forum_id,
-                     :forum_api_key => self.forum_api_key(forum_id),
-                     :limit => limit
-                   })["message"]
+  def thread_list(limit=25)
+    structify(self.class.get("/get_thread_list", :query => {
+                               :forum_id => self.forum_id,
+                               :forum_api_key => self.forum_api_key,
+                               :limit => limit
+                             })["message"])
   end
 
-  def thread_by_url(url, forum_id=self.forum_id)
-    self.class.get("/get_thread_by_url", :query => {
-                     :forum_api_key => self.forum_api_key(forum_id),
-                     :url => url
-                   })["message"]
+  def thread_by_url(url)
+    structify(self.class.get("/get_thread_by_url", :query => {
+                               :forum_api_key => self.forum_api_key,
+                               :url => url
+                             })["message"])
   end
 
-  def post_count_for_threads(forum_id=self.forum_id, *thread_ids)
-    self.class.get("/get_num_posts", :query => {
-                     :thread_ids => thread_ids.join(','),
-                     :forum_api_key => self.forum_api_key(forum_id),
-                   })["message"]
+  def post_count_for_threads(*thread_ids)
+    structify(self.class.get("/get_num_posts", :query => {
+                               :thread_ids => thread_ids.join(','),
+                               :forum_api_key => self.forum_api_key
+                             })["message"])
   end
 
-  def get_thread_posts(thread_id, forum_id=self.forum_id)
-    self.class.get("/get_thread_posts", :query => {
-                     :forum_api_key => self.forum_api_key(forum_id),
-                     :thread_id => thread_id
-                   })
+  def get_thread_posts(thread_id)
+    structify(self.class.get("/get_thread_posts", :query => {
+                               :forum_api_key => self.forum_api_key,
+                               :thread_id => thread_id
+                             })["message"])
   end
 
-  def update_thread(thread_id, forum_id=self.forum_id, opts={})
+  def update_thread(thread_id, opts={})
     body = opts.merge({
-                        :forum_api_key => self.forum_api_key(forum_id),
+                        :forum_api_key => self.forum_api_key,
                         :thread_id => thread_id
                       })
-    self.class.post("/update_thread/", :body => body)["message"]
+    OpenStruct.new(self.class.post("/update_thread/", :body => body))
   end
 
-  def thread_by_identifier(identifier, title, forum_id=self.forum_id)
-    self.class.post("/thread_by_identifier/", :query => {
-                      :identifier => identifier,
-                      :title => title,
-                      :forum_api_key => self.forum_api_key(forum_id)
-                    })["message"]
+  def thread_by_identifier(identifier, title=nil)
+    OpenStruct.new(self.class.post("/thread_by_identifier/", :body => {
+                                :identifier => identifier,
+                                :title => title,
+                                :forum_api_key => self.forum_api_key
+                              })["message"])
   end
 
-
-  def reset!
-    @forum_list = @threads = @forum_api_key = nil
+  def structify(thing)
+    thing.map do |x|
+      x["ID"] = x["id"]
+      OpenStruct.new(x)
+    end
   end
 end
